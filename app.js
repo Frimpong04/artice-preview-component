97ac2a039d77b9ca644c821ea14376ac864cb99c
Frimpong Boateng <executivekfb27@gmail.com>
2023-01-28 16:31:19 +0000

    complete mobile design

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 //SELECTORS
 const shareDivBtn = document.querySelector(".share-div");
-const authorDiv = document.querySelector(".author-div");
-const activeShareBtn = document.querySelector(".share-div-2");
+// const authorDiv = document.querySelector(".author-div");
+// const activeShareBtn = document.querySelector(".share-div-2");
 const activeShareDiv = document.querySelector(".share-div-active");
+const shareIcon = document.querySelector(".share-icon");
+const shareDiv = document.querySelector(".share-div");
 
 //FUNCTION DEFINITIONS
 function showShareLinks() {
     console.log("I have been clicked");
-    authorDiv.classList.add("show-author-div");
-    activeShareDiv.style.display = "flex";
+    activeShareDiv.classList.toggle("show-share-div-active");
+    shareIcon.classList.toggle("share-icon-active");
+    shareDiv.classList.toggle("share-div-active-bg");
 }
 
-function closeShareLinks() {
-    console.log("I Have been closed");
-    authorDiv.style.display = "flex";
-    activeShareDiv.style.display = "none";
-}
 
 //EVENT LISTENERS
 shareDivBtn.addEventListener("click", showShareLinks);
-activeShareBtn.addEventListener("click", closeShareLinks);
